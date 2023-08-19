4e38126fb133e90da3a2ee7dcce7b169d2afc96e
Antonio K (ejtun91) <ejtun91@gmail.com>
2023-08-19 17:48:24 +0200

    adj env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mini-trello-clone-back.onrender.com/api',
-  socketUrl: 'https://mini-trello-clone-back.onrender.com/',
+  apiUrl: 'https://mini-trello-clone-back-m71m-dev.fl0.io/api',
+  socketUrl: 'https://mini-trello-clone-back-m71m-dev.fl0.io/',
 };
