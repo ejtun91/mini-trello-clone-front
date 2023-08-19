3dad11ecf8af43eb073dba814da0ad4044df3c70
Antonio K (ejtun91) <ejtun91@gmail.com>
2023-08-19 14:09:54 +0200

    rm tokrn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,14 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    let token = this.authService.getToken();
-    token &&
-      this.authService.getCurrentUser().subscribe({
-        next: (currentUser) => {
-          this.socketService.setupSocketConnection(currentUser);
-          this.authService.setCurrentUser(currentUser);
-        },
-        error: (err) => {
-          this.authService.setCurrentUser(null);
-        },
-      });
+    this.authService.getCurrentUser().subscribe({
+      next: (currentUser) => {
+        this.socketService.setupSocketConnection(currentUser);
+        this.authService.setCurrentUser(currentUser);
+      },
+      error: (err) => {
+        this.authService.setCurrentUser(null);
+      },
+    });
   }
 }
